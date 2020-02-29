ce60530915e36d97a9b537e53023eb9615e510ac
Victor Fontenele <victorvitoria11@hotmail.com>
2020-02-29 17:31:32 -0300

    some initial settings

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        host: '0.0.0.0',
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'style-loader'
+                    }
+                ]
+            },
+            {
+                test: /.*\.(gif | png | jpe?g)$/i,
+                use: {
+                    loader: 'file-loader'
+                }
+            }
+        ]
+    }
+};
